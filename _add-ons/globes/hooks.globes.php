0e27a629e31f59ea3d98483b8d819c21c57389c3
Brett DeWoody <brett@pinpointsoftware.co>
2015-09-07 11:49:57 -0700

    Add ability to use fieldtypes.

--- _add-ons/globes/hooks.globes.php
+++ _add-ons/globes/hooks.globes.php
@@ -2,29 +2,29 @@
 
 class Hooks_globes extends Hooks {
 
-	public function control_panel__add_routes(){
-		$app = \Slim\Slim::getInstance();
+    public function control_panel__add_routes(){
+        $app = \Slim\Slim::getInstance();
 
-		$app->get('/globes', function() use ($app) {
-			authenticateForRole('admin');
-			doStatamicVersionCheck($app);
+        $app->get('/globes', function() use ($app) {
+            authenticateForRole('admin');
+            doStatamicVersionCheck($app);
 
-			Statamic_View::set_templates(array("globes-overview"), __DIR__ . '/templates');
-
-			$data = $this->tasks->getThemeSettings();
-
-			$app->render(null, array('route' => 'globes', 'app' => $app) + $data);
-
-		})->name('globes');
-		
-		
-		// Update global vars
-		$app->post('/globes/update', function() use ($app) {
-			authenticateForRole('admin');
-			doStatamicVersionCheck($app);
+            Statamic_View::set_templates(array("globes-overview"), __DIR__ . '/templates');
 
             $data = $this->tasks->getThemeSettings();
-            $vars = Request::fetch('globals');
+
+            $app->render(null, array('route' => 'globes', 'app' => $app) + $data);
+
+        })->name('globes');
+        
+        
+        // Update global vars
+        $app->post('/globes/update', function() use ($app) {
+            authenticateForRole('admin');
+            doStatamicVersionCheck($app);
+
+            $data = $this->tasks->getThemeSettings();
+            $vars = Request::fetch('pageglobals');
 
             foreach ($vars as $name => $var) {
                 foreach ($data['globals'] as $key => $item) {
@@ -36,11 +36,11 @@ class Hooks_globes extends Hooks {
  
             File::put($this->tasks->getThemeSettingsPath(), YAML::dump($data, 1));
 
-			$app->flash('success', Localization::fetch('update_success'));
+            $app->flash('success', Localization::fetch('update_success'));
 
-			$app->redirect($app->urlFor('globes'));
-		});
-	
-	}
+            $app->redirect($app->urlFor('globes'));
+        });
+    
+    }
 
 }
